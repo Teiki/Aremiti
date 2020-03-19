217c4b56aa28b41666513a7ab480cf641648b736
antoinegaltier <antoine.galtier@gmail.com>
2020-03-18 19:32:47 -1000

    Possibility to change date with DatePickerDialog

--- app/src/main/java/fr/teiki/aremiti/ui/home/HomeFragment.java
+++ app/src/main/java/fr/teiki/aremiti/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package fr.teiki.aremiti.ui.home;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -24,10 +27,11 @@ import fr.teiki.aremiti.holder.ScheduleOfOneDay;
 import fr.teiki.aremiti.network.APITalker;
 import fr.teiki.aremiti.parser.MyScheduleParser;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
 	private HomeViewModel homeViewModel;
 
+	private TextView txt_date;
 	private ListView list_of_path;
 	private RadioGroup radioGroup;
 	private PathListAdapter pathListAdapter;
@@ -36,25 +40,55 @@ public class HomeFragment extends Fragment {
 							 ViewGroup container, Bundle savedInstanceState) {
 		homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
 		View root = inflater.inflate(R.layout.fragment_home, container, false);
-		//final TextView textView = root.findViewById(R.id.text_home);
-//		homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//			@Override
-//			public void onChanged(@Nullable String s) {
-//				textView.setText(s);
-//			}
-//		});
 
 		list_of_path = root.findViewById(R.id.list_of_path);
 		radioGroup = root.findViewById(R.id.radio_group);
+		txt_date = root.findViewById(R.id.txt_date);
+		final Button btn_date_picker = root.findViewById(R.id.btn_date);
+		btn_date_picker.setOnClickListener(this);
+
 
 		refreshData();
 		return root;
 	}
 
+	private void displayCalendarDialog(){
+		Calendar c = Calendar.getInstance();
+		c.setTime(homeViewModel.getScheduleOfOneDay().getDate());
+		int mYear = c.get(Calendar.YEAR);
+		int mMonth = c.get(Calendar.MONTH);
+		int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+		DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+				new DatePickerDialog.OnDateSetListener() {
+
+					@Override
+					public void onDateSet(DatePicker view, int year,
+										  int monthOfYear, int dayOfMonth) {
+						Calendar c = Calendar.getInstance();
+						c.set(year,monthOfYear,dayOfMonth);
+						refreshData(c.getTime());
+					}
+				}, mYear, mMonth, mDay);
+		datePickerDialog.show();
+	}
+
+	private void refreshText(int dayOfMonth, int monthOfYear, int year){
+		txt_date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+	}
+
 	private void refreshData(){
-		Date date = new Date();
-		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-		cal.setTime(date);
+		refreshData(new Date());
+	}
+	private void refreshData(Date date){
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		int mYear = c.get(Calendar.YEAR);
+		int mMonth = c.get(Calendar.MONTH);
+		int mDay = c.get(Calendar.DAY_OF_MONTH);
+		refreshText(mDay,mMonth,mYear);
+
 		APITalker.getSchedule(getContext(), date, new MyScheduleParser.MyScheduleParserDelegate() {
 			@Override
 			public void onScheduleParsed(ScheduleOfOneDay scheduleOfOneDay) {
@@ -72,5 +106,11 @@ public class HomeFragment extends Fragment {
 				//refreshData(scheduleOfOneDay);
 			}
 		});
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (R.id.btn_date == v.getId())
+			displayCalendarDialog();
 	}
 }
